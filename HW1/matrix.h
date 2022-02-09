f3a4165224d2e44f5cd4e886f5d6c7f816046043
oksana-yashan <yaokvol@gmail.com>
2022-02-09 15:23:07 +0200

    privacy changed

--- HW1/matrix.h
+++ HW1/matrix.h
@@ -16,13 +16,13 @@ public:
     friend Matrix operator-(const Matrix&, const Matrix&);
     friend Matrix operator*(const Matrix&, const Matrix&);
     friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
-    
+ 
+private:
     void alloc();
     void dealloc();
     void check_size_equality(int N1, int M1) const;
     void check_size_for_mult(int N1, int M1) const;
 
-private:
     float** matrix;
     int n_rows, n_col;
 };
